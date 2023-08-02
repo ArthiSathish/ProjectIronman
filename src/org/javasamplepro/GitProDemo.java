a5d1e000c6c455c3ec07b4c6be3b3e47de84d526
Arthisathish <sathishkumar30693@gmail.com>
2023-08-02 14:42:19 +0530

    Third push

--- src/org/javasamplepro/GitProDemo.java
+++ src/org/javasamplepro/GitProDemo.java
@@ -4,6 +4,7 @@ public class GitProDemo {
 	public static void main(String[] args) {
 		System.out.println("Sample Git");
 		System.out.println("Sample Java");
+		System.out.println("Sample Demo");
 		}
 
 }
